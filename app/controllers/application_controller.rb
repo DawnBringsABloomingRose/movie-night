8d1dd256c9af28f8fa5227f03c8ab59b8b4b28c3
DawnBringsABloomingRose <113314828+DawnBringsABloomingRose@users.noreply.github.com>
2023-09-21 15:10:59 -0400

    Add basic login and out capabilities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permiited_parameters, if: :devise_controller?
+    protected
+    def configure_permiited_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :email])
+    end
 end
---
+++ db/migrate/20230921184558_remove_index_from_user_emails.rb
@@ -0,0 +1,5 @@
+class RemoveIndexFromUserEmails < ActiveRecord::Migration[7.0]
+  def change
+    remove_index :users, :email
+  end
+end
