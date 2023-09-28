b226e54a79b6bc2a9a00875219e1c8b9c8ddea85
DawnBringsABloomingRose <113314828+DawnBringsABloomingRose@users.noreply.github.com>
2023-09-28 13:56:42 -0400

    Add devise routes to prevent actions if not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permiited_parameters, if: :devise_controller?
+    before_action :authenticate_user!
     protected
     def configure_permiited_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :email])
